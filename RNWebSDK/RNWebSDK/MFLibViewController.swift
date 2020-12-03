4cac65c319038f9304819ffacbcfc1b2e4d8201c
Prashant Dwivedi <dwi.pra24@gmail.com>
2020-12-03 16:06:06 +0530

    added camera method

--- RNWebSDK/RNWebSDK/MFLibViewController.swift
+++ RNWebSDK/RNWebSDK/MFLibViewController.swift
@@ -9,11 +9,28 @@
 import Foundation
 import UIKit
 
-public class MFLibViewController: UIViewController {
+public class MFLibViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     
     public static func appendString(str:String) -> String {
         return "Hello" + str
+    }
+    
+    public static func openNativeCamera() {
+        
+        print("Inside Open Camera")
+        let vc = UIImagePickerController()
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            vc.sourceType = .camera
+        }else {
+            vc.sourceType = .photoLibrary
+        }
+        
+        vc.allowsEditing = true
+        let weakSelf = MFLibViewController()
+        vc.delegate = weakSelf
+        weakSelf.present(vc, animated: true)
     }
     
     
