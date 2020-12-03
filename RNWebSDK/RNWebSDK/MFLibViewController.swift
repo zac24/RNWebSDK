b24f74911ac4313b550ac475263b52ec5588e318
Prashant Dwivedi <dwi.pra24@gmail.com>
2020-12-03 13:23:19 +0530

    created basic framework setup

---
+++ RNWebSDK/RNWebSDK/MFLibViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MFLibViewController.swift
+//  RNWebSDK
+//
+//  Created by Prashant Dwivedi on 03/12/20.
+//  Copyright © 2020 Prashant Dwivedi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class MFLibViewController: UIViewController {
+    
+    
+    public static func appendString(str:String) -> String {
+        return "Hello" + str
+    }
+    
+    
+    
+}
---
+++ RNWebSDK/RNWebSDK/RNWebSDK.h
@@ -0,0 +1,19 @@
+//
+//  RNWebSDK.h
+//  RNWebSDK
+//
+//  Created by Prashant Dwivedi on 03/12/20.
+//  Copyright © 2020 Prashant Dwivedi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RNWebSDK.
+FOUNDATION_EXPORT double RNWebSDKVersionNumber;
+
+//! Project version string for RNWebSDK.
+FOUNDATION_EXPORT const unsigned char RNWebSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNWebSDK/PublicHeader.h>
+
+
